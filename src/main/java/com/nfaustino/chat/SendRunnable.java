62ef419572a7d4597ca6c9648b5d1025973372d1
Igor Neves Faustino <igornfaustino@gmail.com>
2018-10-09 19:20:47 -0300

    remove unused import

--- src/main/java/com/nfaustino/chat/SendRunnable.java
+++ src/main/java/com/nfaustino/chat/SendRunnable.java
@@ -10,7 +10,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
